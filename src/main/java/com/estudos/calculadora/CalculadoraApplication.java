ef88ad2ba86d804913a7ac4125e4c6142174a8ec
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-11 19:37:59 -0300

    :lipstick: feat: Adicionar construtor para setar configuração visual inicial da calculadora

--- src/main/java/com/estudos/calculadora/CalculadoraApplication.java
+++ src/main/java/com/estudos/calculadora/CalculadoraApplication.java
@@ -3,8 +3,19 @@ package com.estudos.calculadora;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
-public class CalculadoraApplication {
+public class CalculadoraApplication extends JFrame {
+
+    public CalculadoraApplication(){
+
+        setSize(417,579);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CalculadoraApplication.class, args);
