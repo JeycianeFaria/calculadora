6d0866e6ce971f9375f9875b412bfa03657e51a6
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-13 12:21:06 -0300

    :lipstick: feat: Configurar layout do display

--- src/main/java/com/estudos/calculadora/view/Display.java
+++ src/main/java/com/estudos/calculadora/view/Display.java
@@ -1,4 +1,26 @@
 package com.estudos.calculadora.view;
 
-public class Display {
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JPanel {
+
+    //TODO pesquisar como diminuir tamanho dos numeros quando vamso adicionando muitos no display.
+
+    private final JLabel label;
+
+    public Display() {
+
+        setBackground(new Color(40, 44, 52));
+
+        label = new JLabel("12345");
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("courier", Font.PLAIN, 60));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 70));
+
+        add(label);
+
+    }
+
 }
