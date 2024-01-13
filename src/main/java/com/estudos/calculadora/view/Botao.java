7eb3a50fd50839d804e047e1adef0bf4ea3d9b95
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-13 14:26:57 -0300

    :lipstick: feat: Configurar layout do botao

--- src/main/java/com/estudos/calculadora/view/Botao.java
+++ src/main/java/com/estudos/calculadora/view/Botao.java
@@ -1,6 +1,18 @@
 package com.estudos.calculadora.view;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Botão extends JButton {
+public class Botao extends JButton {
+
+    public Botao(String texto, Color cor) {
+
+        setText(texto);
+        setFont(new Font("courier", Font.PLAIN, 50));
+        setOpaque(true);
+        setBackground(cor);
+        setForeground(Color.WHITE);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+    }
 }
