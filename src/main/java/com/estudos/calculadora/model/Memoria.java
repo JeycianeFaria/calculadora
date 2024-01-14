8c461d09c4975c089e36a1be051c5b3bd061eb0c
Jeyciane Faria <jeycianedeveloper@gmail.com>
2024-01-14 11:33:19 -0300

    :construction: feat: Categorizar eventos que ocorrem na calculadora

--- src/main/java/com/estudos/calculadora/model/Memoria.java
+++ src/main/java/com/estudos/calculadora/model/Memoria.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Memoria {
 
+    private enum TipoComando {
+        ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA;
+    }
+
     private static final Memoria instancia = new Memoria();
     private final List<MemoriaObservador> observadores = new ArrayList<>();
     private String textoAtual = "";
@@ -24,15 +28,52 @@ public class Memoria {
         observadores.add(observador);
     }
 
-    public void processarComando(String valor) {
+    public void processarComando(String texto) {
 
-        if (valor.equals("AC")) {
+        TipoComando tipoComando = detectarTipoComando(texto);
+
+        if (texto.equals("AC")) {
             textoAtual = "";
         } else {
-            textoAtual += valor;
+            textoAtual += texto;
         }
 
         observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+
+    }
+
+    //TODO REFATORAR ESSE METODO
+    private TipoComando detectarTipoComando(String texto) {
+
+        if (textoAtual.isEmpty() && texto.equals("0")) {
+            return null;
+        }
+
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        } catch (NumberFormatException e) {
+
+            if (texto.equals("AC")) {
+                return TipoComando.ZERAR;
+            } else if (texto.equals("/")) {
+                return TipoComando.DIV;
+            } else if (texto.equals("*")) {
+                return TipoComando.MULT;
+            } else if (texto.equals("+")) {
+                return TipoComando.SOMA;
+            } else if (texto.equals("-")) {
+                return TipoComando.SUB;
+            } else if (texto.equals("=")) {
+                return TipoComando.IGUAL;
+            } else if (texto.equals(",")) {
+                return TipoComando.VIRGULA;
+            }
+
+
+        }
+
+        return null;
 
     }
 
